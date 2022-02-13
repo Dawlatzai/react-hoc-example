14744bd600cdc89e3354a05df13299dd46d54cf2
dawlatzai <dawlatzai.ghousi@gmail.com>
2022-02-13 23:58:17 +0430

    Add sass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
 
+import UserProfile from './components/user-profile/user-profile.component'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <UserProfile name="Ali" email="Ali@email.com" />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
